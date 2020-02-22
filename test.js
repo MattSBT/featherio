b28f6429819f402afd559e7e95c4311abe78985a
Mathew Eickmeyer <33883401+MattSBT@users.noreply.github.com>
2020-02-22 15:45:27 -0500

    Tests

--- test.js
+++ test.js
@@ -1,11 +1,2 @@
 
 
-
-test("Testing API endpoint", async done => {
-  const encode = {
-      Shift = 8,
-      Message = "check it out"
-  }
-  await request(app).post('/api/encode').send(encode);
-  done();
-});
