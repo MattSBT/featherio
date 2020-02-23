66ad1a0b8896a2503448ac3cca8d9e4152eb09ee
Mathew Eickmeyer <33883401+MattSBT@users.noreply.github.com>
2020-02-23 10:25:54 -0500

    Made Test feature for POST failure

--- Challenge 1/test.js
+++ Challenge 1/test.js
@@ -1,2 +1,11 @@
+const app = require("./index.js");
+const supertest = require("supertest");
+const request = supertest(app);
 
+it("POSTS to /api/encode endpoint to check for failure response", async done => {
+  const response = await request.post("/api/encode");
 
+  expect(response.status).toBe(500);
+  expect(response.body.message).toBe("");
+  done();
+});
