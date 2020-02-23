15a3d1721cf6764d0193914c00185f3f6155d3ea
Mathew Eickmeyer <33883401+MattSBT@users.noreply.github.com>
2020-02-23 10:53:36 -0500

    Added base cases and cleaned up code a bit

--- Challenge 1/routes/routes.js
+++ Challenge 1/routes/routes.js
@@ -12,25 +12,45 @@ function shiftChar(char, shiftCypher, { alphaOnly = false } = {}) {
   }
 }
 
+function createCypherMessage(message, cypher) {
+  let tempCypherMessage = [];
+
+  //Looks for base cases or Shifts each char and pushes to array before responding back
+  if (message.length <= 1) {
+    tempCypherMessage = shiftChar(message, cypher);
+  } else if (cypher === 0) {
+    tempCypherMessage = message;
+  } else {
+    for (let i of message) {
+      if (i.includes(" ")) {
+        tempCypherMessage.push(i);
+      } else {
+        tempCypherMessage.push(shiftChar(i, cypher));
+      }
+    }
+    tempCypherMessage = tempCypherMessage.join("");
+  }
+  return tempCypherMessage;
+}
+
 module.exports = app => {
   app.post("/api/encode", (req, res) => {
     let shiftCypher = parseInt(req.query.Shift);
-    let message = req.query.message;
-    if (!shiftCypher || !message) {
+    let message = req.query.Message;
+    if (
+      !shiftCypher ||
+      message === "" ||
+      typeof shiftCypher !== "number" ||
+      typeof message !== "string"
+    ) {
       res.status(500).send("");
     } else {
-      cypherMessage = [];
-
-      //Shifts each char and pushes to array before responding back
-      for (let i of message) {
-        if (i.includes(" ")) {
-          cypherMessage.push(i);
-        } else {
-          cypherMessage.push(shiftChar(i, shiftCypher));
-        }
+      try {
+        let cypherMessage = createCypherMessage(message, shiftCypher);
+      } catch {
+        console.log("This message could not be cyphered");
+        res.status(500).send("");
       }
-      cypherMessage = cypherMessage.join("");
-
       // Append to file on server
       fs.appendFile("cypherMessage.txt", cypherMessage, error => {
         if (error) throw error;
--- Challenge 1/test.js
+++ Challenge 1/test.js
@@ -3,9 +3,20 @@ const supertest = require("supertest");
 const request = supertest(app);
 
 it("POSTS to /api/encode endpoint to check for failure response", async done => {
-  const response = await request.post("/api/encode");
+  const response = await request.post("/api/encode").send({
+    Message: "",
+    Shift: ""
+  });
 
   expect(response.status).toBe(500);
   expect(response.body.message).toBe("");
   done();
+});
+it("POSTS to api/encode endpoint testing encode non alpha", async done => {
+  const response = await request.post("/api/encode").send({
+    Message: "dad",
+    Shift: "2"
+  });
+  expect(response.status).toBe(200);
+  expect(response.body.message).toBe("gdg");
 });
