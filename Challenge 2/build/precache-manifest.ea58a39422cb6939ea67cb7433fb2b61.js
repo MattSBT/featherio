d42f434cddee6c1eec58a2fa4cade69d150b353b
Mathew Eickmeyer <33883401+MattSBT@users.noreply.github.com>
2020-02-23 08:39:45 -0500

    Adding Challenge 2 production

---
+++ Challenge 2/build/precache-manifest.ea58a39422cb6939ea67cb7433fb2b61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0a5dd4630b9e7944238a77763b944f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e0bbe270a35646d2b0a",
+    "url": "/static/css/main.d7996775.chunk.css"
+  },
+  {
+    "revision": "c7eee8405eb02f803da3",
+    "url": "/static/js/2.8227523b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8227523b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0bbe270a35646d2b0a",
+    "url": "/static/js/main.900e2aae.chunk.js"
+  },
+  {
+    "revision": "3d2785cfd6e280812ac4",
+    "url": "/static/js/runtime-main.ca89616d.js"
+  },
+  {
+    "revision": "c3c3450f599dc589e44649f10e974409",
+    "url": "/static/media/featherbackground.c3c3450f.png"
+  }
+]);
